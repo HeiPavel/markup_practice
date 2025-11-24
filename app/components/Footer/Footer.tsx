fc9ba899a0a40345823df6d8a9948656a7746b65
HeiPavel <112711905HeiPavel@users.noreply.github.com>
2025-11-24 16:21:47 +0200

    Add subscribtion form

--- app/components/Footer/Footer.tsx
+++ app/components/Footer/Footer.tsx
@@ -2,9 +2,12 @@ import { Newsletter } from './Newsletter'
 
 export function Footer() {
   return (
-    <section className='bg-turquoise-medium'>
+    <section 
+      id='contact'
+      className='bg-turquoise-medium'
+    >
       <div className='box-container flex justify-center pt-9 laptop:pt-12 pb-[74px]'>
-        <div className='grow px-6 tablet:px-20 max-w-[1650px]'>
+        <div className='grow px-6 md:px-20 max-w-[1650px]'>
           <Newsletter/>
           <div className='px-6 mt-14 flex justify-between'>
 
--- app/components/Footer/Form.tsx
+++ app/components/Footer/Form.tsx
@@ -1,8 +1,10 @@
 'use client'
 
+import { useState, useEffect } from 'react'
 import { useForm, SubmitHandler } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
 import {z} from 'zod'
+import { ErrorMessage } from '@hookform/error-message'
 
 const schema = z.object({
   email: z.string()
@@ -14,11 +16,14 @@ const schema = z.object({
 type EmailInput = z.infer<typeof schema>
 
 export function Form() {
+  const [isFocused, setIsFocused] = useState(false)
+
   const {
     register,
     handleSubmit,
     formState: {
-      errors
+      errors,
+      isSubmitSuccessful
     },
     reset
   } = useForm<EmailInput>({
@@ -28,23 +33,37 @@ export function Form() {
 
   const onSubmit: SubmitHandler<EmailInput> = (data) => {
     console.log(data)
-    reset()
   }
+
+  useEffect(() => {
+    if (isSubmitSuccessful) reset()
+  }, [isSubmitSuccessful, reset])
 
   return (
     <form 
       onSubmit={handleSubmit(onSubmit)}
-      className='h-14 tablet:h-[76px] w-full laptop:max-w-[516px] flex gap-3 laptop:gap-4 
-        p-2 tablet:p-3 rounded-full bg-white font-jakarta'
+      className={`h-14 tablet:h-[76px] w-full laptop:max-w-[516px] flex gap-3 laptop:gap-4 
+        p-2 tablet:p-3 rounded-full bg-white font-jakarta outline-2 
+        ${errors.email ? 'outline-red' : isFocused ? 'outline-blue-gradient' : 'outline-transparent'} 
+        transition-colors ease-in-out duration-150`}
     >
-      <div className='h-full grow'>
+      <div className='relative h-full grow'>
         <input
           type='text'
           {...register('email')}
           placeholder='Email address'
-          className='size-full pl-2 tablet:pl-4 text-lg leading-7 placeholder:text-gray-medium outline-0 text-black bg-white transparent-autofill'
+          className='relative overflow-x-hidden size-full pl-2 tablet:pl-4 text-lg leading-7 
+            placeholder:text-gray-medium outline-0 text-black bg-white transparent-autofill'
+          onFocus={() => setIsFocused(true)}
+          onBlur={() => setIsFocused(false)}
         />
-        <span className='text-red-500 text-sm'>{errors.email ? errors.email.message : ''}</span>
+        <ErrorMessage
+          errors={errors}
+          name='email'
+          render={({message}) => (
+            <p className='absolute -bottom-6 tablet:-bottom-8 left-2 tablet:left-4 text-xs tablet:text-sm text-red'>{message}</p>
+          )}
+        />
       </div>
       <button
         type='submit' 
