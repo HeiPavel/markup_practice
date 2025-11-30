10032d31360766bc092379bc334bbacd48672f00
HeiPavel <112711905HeiPavel@users.noreply.github.com>
2025-11-30 13:00:43 +0200

    Styled snackbar

--- app/components/Footer/SubscribeConfirmation.tsx
+++ app/components/Footer/SubscribeConfirmation.tsx
@@ -1,8 +1,9 @@
 'use client'
 
-import { SyntheticEvent, useContext } from 'react'
-import { Snackbar, SnackbarOrigin, Slide, SlideProps, SnackbarCloseReason } from '@mui/material'
+import { useContext } from 'react'
+import { Snackbar, SnackbarOrigin, Slide, SlideProps } from '@mui/material'
 import { FormContext } from './FormData'
+import {ReactComponent as CheckIcon} from '@/public/icons/check_icon.svg'
 
 const position: SnackbarOrigin = {
   vertical: 'bottom',
@@ -10,13 +11,22 @@ const position: SnackbarOrigin = {
 }
 
 const SlideMessage = (props: SlideProps) => {
-    return (
-      <Slide 
-        {...props} 
-        direction='left'
-      />
-    )
-  }
+  return (
+    <Slide 
+      {...props} 
+      direction='left'
+    />
+  )
+}
+
+const Message = () => {
+  return (
+    <div className='flex items-center gap-2 font-jakarta text-white leading-7'>
+      <CheckIcon className='size-5'/>
+      <p>Subscribed successfully</p>
+    </div>
+  )
+}
 
 export function SubscribeConfirmation() {
   const {open, setOpen} = useContext(FormContext)
@@ -28,9 +38,17 @@ export function SubscribeConfirmation() {
       open={open}
       anchorOrigin={position}
       onClose={handleClose}
-      autoHideDuration={1000000}
-      message='Subscription seccessful'
-      slots={{transition: SlideMessage}}
+      autoHideDuration={3000}
+      message={<Message/>}
+      slots={{
+        transition: SlideMessage
+      }}
+      className='min-w-72 !left-auto'
+      slotProps={{
+        content: {
+          className: '!bg-green'
+        }
+      }}
     >
     </Snackbar>
   )
